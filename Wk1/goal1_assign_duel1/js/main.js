16e932d6b8609bd20121d8c9ea11caab2760de72
sgehrke <shaungehrke@shauns-mbp.eau.wi.charter.com>
2014-02-05 22:16:31 -0600

    Cleaning up original alerts
    As I started to write the program I needed some help, so I started
    watching the video. This is when I cleaned up the code a bit.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -6,65 +6,39 @@ var p1Health = 100;
 var p2Name = "Kratos";
 var p2Health = 100;
 
-var p1maxDamage = 50;
-var p2maxDamage = 50;
+var p1maxDamage = 15;
+var p2maxDamage = 15;
 
-var p1minDamage = 25;
-var p2minDamage = 25;
+var p1minDamage = p1maxDamage * .5;
+var p2minDamage = p2maxDamage * .5;
 
 
-var round = 1;
+var round = 0;
 
 function fight(){
 	console.log ("this is the function fight");
+	
 	alert(p1Name + ":" + p1Health + " *START* " + p2Name + ":" + p2Health);
 
 	for (i=0; i<10; i++){
 		var p1Damage = Math.floor(Math.random()*(p1maxDamage - p1minDamage) + p1minDamage);
 		var p2Damage = Math.floor(Math.random()*(p2maxDamage - p2minDamage) + p2minDamage);
 		
-		
-		
-		
-		console.log (p1Damage);
-		console.log (p1Damage);
+		p1Health -= p1Damage;
+		p2Health -= p2Damage;
+	//	console.log (p1Damage);
+	//	console.log (p1Damage);
+	//	console.log ("p1: " + p1Health);
+	//	console.log ("p2: " + p2Health);
+		round++
+		console.log(p1Name + ":" + p1Health + " *ROUND " + round + " OVER* " + p2Name + ":" + p2Health);
 
-	}
-	winnerCheck()
-}
-
-function winnerCheck(){
-	
-}
+		var result = winnerCheck();
+		console.log (result);
+		
 
 console.log ("start program");
-fight()
+fight();
 
-/*
-alert("Kabal:  *ROUND 1 OVER* Kratos: ");
 
-alert("Kabal:  *ROUND 2 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 3 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 4 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 5 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 6 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 7 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 8 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 9 OVER* Kratos: ");
-
-alert("Kabal:  *ROUND 10 OVER* Kratos: ");
-
-alert("GAME OVER Kratos Wins!!!");
-
-alert("GAME OVER Kabal Wins!!!");
-
-alert("NO WINNER:("); //if both players die
-*/
 
