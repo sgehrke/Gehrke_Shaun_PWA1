d0af1d2cebba0e12c30535513688ee950dc261a0
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-02-11 11:39:21 -0600

    solved problem 2
    used a function containing concat to concatenate two string literals
    together to create a full name

--- Wk1/goal2_assign_jspractice/js/main.js
+++ Wk1/goal2_assign_jspractice/js/main.js
@@ -4,9 +4,11 @@ Date: 02-11-14
 Assignment: Goal2 - jspractice
 */
 
-var arrayNumbers = [1,4,6,8,2,7,8,2,5,9]
 
-function avgNumber(array){
+//problem 1 'avgNumbers' 
+var arrayNumbers = [1,1,6,8,2,7,8,2,5,9]
+
+function avgNumbers(array){
 	var total = 0;
 	for (var i = 0, x = array.length; i < x; i++) {
 	    total += array[i];
@@ -15,5 +17,19 @@ function avgNumber(array){
 	return average;	
 }
 
-var average = avgNumber(arrayNumbers);
+var average = avgNumbers(arrayNumbers);
 console.log (average);
+
+//problem 2 'fullName'
+var firstName = "Shaun ";
+var lastName = "Gehrke ";
+
+function fullName(first, last){
+	var combName = first.concat(last);
+	return combName;
+}
+
+var name = fullName(firstName, lastName);
+console.log (name);
+
+console.log (typeof (name));
