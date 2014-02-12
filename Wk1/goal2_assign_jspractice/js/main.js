b31e93d27ad3e51be0c408cc71c4cfe3b68fff65
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-02-11 20:54:28 -0600

    Problem 6
    Followed the directions to create a false boolean and an return an
    array as specified

--- Wk1/goal2_assign_jspractice/js/main.js
+++ Wk1/goal2_assign_jspractice/js/main.js
@@ -109,34 +109,14 @@ create a function named 'vowelsInWord'
 
 
 console.log("***************************************************problem5")
-var word = "aeiou";
-
-/*
-
-function vowelInWord(str) {
-	console.log (str)
-    return --(str.split(/[aeiou]/).length);
-}
-
-var vow = vowelInWord(word);
-console.log (vow);
-*/
-
+var word = "vowel";
 var counter = 0
-console.log (counter);
-
 
 function vowelsInWord(vowels){
 
-	var counter = 0
-	console.log (counter);
-	
-	
 	for ( var i=0, x=vowels.length; i<x; i++){
-	var vowelsArray = vowels.split("");
-	console.log(vowelsArray)
-	console.log(vowelsArray[i])
-		
+		var vowelsArray = vowels.split("");
+	
 		switch (vowelsArray[i]) {
 			case "a": 
 				counter++;
@@ -165,7 +145,8 @@ console.log (vowelCount);
 
 
 
-/******************************************************* problem 6
+console.log("********************************************** problem 6");
+/*
 create a function named 'findNum'
 - accepts 2 parameters into the function - 1. array of numbers, 2. boolean
 - if the second parameter being passed is "false" or null then
@@ -173,23 +154,26 @@ create a function named 'findNum'
 - else
  - create an array with all of the even numbers from the array
 - return the array
-- console.log the answer outside of the function
+
 */
-/*
+
 var array = [6,8,3,2,2,8,0];
-var falseBoolean = if (false){
-	var arrayOdd = [1,3,5,7,9]
-} else {
-	var arrEven = [2,4,6,8,0];
-}
+var falseBoolean = true;
 
 function findNum(a,b){
-	return foundNum;
+	if (b === false){
+	var b = [2,4,6,8];
+	return b;
+	} else {
+		var b = [1,3,5,7,9];
+		return b;
+	}
+	
 	
 }
 
 var foundNum = findNum(array, falseBoolean);
 console.log (foundNum);
-*/
+
 
 
